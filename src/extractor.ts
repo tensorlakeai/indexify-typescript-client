03d87fdd3cfcd02fc14740afc27e4cf6dca66399
Lucas Jackson <lucasjackson5815@gmail.com>
2024-04-01 13:34:35 -0700

    add input_mime_types to extractor type

--- src/extractor.ts
+++ src/extractor.ts
@@ -2,12 +2,14 @@ import { IExtractor, IExtractorSchema } from "./types";
 
 class Extractor {
   public name: string;
+  public input_mime_types: string[];
   public description: string;
   public input_params: Record<string, string | number>;
   public outputs: IExtractorSchema;
 
   constructor(data: IExtractor) {
     this.name = data.name;
+    this.input_mime_types = data.input_mime_types;
     this.description = data.description;
     this.input_params = data.input_params;
     this.outputs = data.outputs;
